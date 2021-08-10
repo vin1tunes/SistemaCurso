79b658ac8e8acc0a17c4ccada98bdac71bf22ad5
Vinicius <vinicius_antunes7@hotmail.com>
2021-08-10 20:24:10 -0300

    Teste

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
 # Listar os alunos da Disciplina;
 # Listar as atividades da Disciplina;
 # Listar todas as atividades de um aluno em uma disciplina; AtividadeAluno tem uma Disciplina?
+# Teste
