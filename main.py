bcf39bb27e26f3ba380070c86d349d84cca4a4bd
Vinicius <vinicius_antunes7@hotmail.com>
2021-09-18 17:25:52 -0300

    Correção feita nos controladores.

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from controle.controlador_sistema import ControladorSistema
 
 if __name__ == "__main__":
     ControladorSistema().inicializa_sistema()
+
+# tela_aluno, controlador_alunos => OK!
+# tela_professor, controlador_professores =>OK!
+# tela_atividade, controlador_atividades => OK!
+# tela_disciplina, controlador_disciplinas => OK!
+# tela_atividade_aluno, controlador_atividades_aluno => OK!
